e912259fa5c29ec970a1e18c742a086959d5635b
conor heffernan <conorheffernan@hubspot.com>
2021-02-10 19:37:38 +0000

    tidied up app.js

--- App.js
+++ App.js
@@ -1,27 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useEffect, useState } from "react";
-import {
-	SafeAreaView,
-	StyleSheet,
-	Text,
-	View,
-	Image,
-	Dimensions,
-} from "react-native";
-import {
-	useDimensions,
-	useDeviceOrientation,
-} from "@react-native-community/hooks";
-import LoginScreen from "./app/Screens/LoginScreen";
-import ViewImage from "./app/Screens/ViewImage";
-import UIButton from "./app/components/UIButton";
-import UICard from "./app/components/UICard";
-import SampleComponent from "./app/components/SampleComponent";
-import DetailsScreen from "./app/Screens/DetailsScreen";
-import MessagesScreen from "./app/Screens/MessagesScreen";
-import MyAccountScreen from "./app/Screens/MyAccountScreen";
-import ListingsScreen from "./app/Screens/ListingsScreen";
-import UIInput from "./app/components/UIInput";
+import { StyleSheet } from "react-native";
 import UIPicker from "./app/components/UIPicker";
 import UIScreen from "./app/components/UIScreen";
 import colors from "./app/config/colors";
@@ -50,15 +29,4 @@ export default function App() {
 	);
 }
 
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#F5F8FA",
-		alignItems: "center",
-		justifyContent: "center",
-	},
-	logoStyle: {
-		height: 50,
-		width: 50,
-	},
-});
+const styles = StyleSheet.create({});
